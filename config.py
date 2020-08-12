46eeb7e7aa2be42fc7e12381b4a34601c5964260
어진솔 <jinsol@agilesoda.ai>
2020-08-12 13:14:43 +0900

    Update paths

--- config.py
+++ config.py
@@ -1,19 +1,20 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import argparse
+import math
+import os.path as osp
+import os
+import six
+
 import sys
 sys.path.append('./')
-
-import six
-import os
-import os.path as osp
-import math
-import argparse
 
 
 parser = argparse.ArgumentParser(description="Softmax loss classification")
 # data
-parser.add_argument('--synthetic_train_data_dir', nargs='+', type=str, metavar='PATH')
+parser.add_argument('--synthetic_train_data_dir',
+                    nargs='+', type=str, metavar='PATH')
 parser.add_argument('--test_data_dir', type=str, metavar='PATH')
 parser.add_argument('-b', '--batch_size', type=int, default=128)
 parser.add_argument('-j', '--workers', type=int, default=8)
@@ -44,11 +45,12 @@ parser.add_argument('--max_len', type=int, default=100)
 parser.add_argument('--n_group', type=int, default=1)
 parser.add_argument('--STN_ON', action='store_true',
                     help='add the stn head.')
-parser.add_argument('--tps_margins', nargs='+', type=float, default=[0.05,0.05])
+parser.add_argument('--tps_margins', nargs='+',
+                    type=float, default=[0.05, 0.05])
 parser.add_argument('--stn_activation', type=str, default='none')
 parser.add_argument('--num_control_points', type=int, default=20)
 parser.add_argument('--stn_with_dropout', action='store_true', default=False)
-## lstm
+# lstm
 parser.add_argument('--with_lstm', action='store_true', default=False,
                     help='whether append lstm after cnn in the encoder part.')
 parser.add_argument('--decoder_sdim', type=int, default=512,
@@ -60,9 +62,10 @@ parser.add_argument('--lr', type=float, default=1.0,
                     help="learning rate of new parameters, for pretrained "
                          "parameters it is 10 times smaller than this")
 parser.add_argument('--momentum', type=float, default=0.9)
-parser.add_argument('--weight_decay', type=float, default=0.0) # the model maybe under-fitting, 0.0 gives much better results.
+# the model maybe under-fitting, 0.0 gives much better results.
+parser.add_argument('--weight_decay', type=float, default=0.0)
 parser.add_argument('--grad_clip', type=float, default=1.0)
-parser.add_argument('--loss_weights', nargs='+', type=float, default=[1,1,1])
+parser.add_argument('--loss_weights', nargs='+', type=float, default=[1, 1, 1])
 # training configs
 parser.add_argument('--resume', type=str, default='', metavar='PATH')
 parser.add_argument('--evaluate', action='store_true',
@@ -76,13 +79,15 @@ parser.add_argument('--cuda', default=True, type=bool,
                     help='whether use cuda support.')
 # testing configs
 parser.add_argument('--evaluation_metric', type=str, default='accuracy')
-parser.add_argument('--evaluate_with_lexicon', action='store_true', default=False)
+parser.add_argument('--evaluate_with_lexicon',
+                    action='store_true', default=False)
 parser.add_argument('--beam_width', type=int, default=5)
 # misc
 working_dir = osp.dirname(osp.dirname(osp.abspath(__file__)))
 parser.add_argument('--logs_dir', type=str, metavar='PATH',
                     default=osp.join(working_dir, 'logs'))
-parser.add_argument('--real_logs_dir', type=str, metavar='PATH', default='logs/aster.pytorch')
+parser.add_argument('--real_logs_dir', type=str,
+                    metavar='PATH', default='logs/aster.pytorch')
 parser.add_argument('--debug', action='store_true',
                     help="if debugging, some steps will be passed.")
 parser.add_argument('--vis_dir', type=str, metavar='PATH', default='',
@@ -90,6 +95,7 @@ parser.add_argument('--vis_dir', type=str, metavar='PATH', default='',
 parser.add_argument('--run_on_remote', action='store_true', default=False,
                     help="run the code on remote or local.")
 
+
 def get_args(sys_args):
-  global_args = parser.parse_args(sys_args)
-  return global_args
+    global_args = parser.parse_args(sys_args)
+    return global_args
